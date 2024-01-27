a1cde4f3dbb73b0b3dca7642a0dd8c3c90dc96c1
scott123180 <me@scotthansen.io>
2024-01-26 21:16:53 -0500

    returning the info from the function

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const { scrape } = require('./scraper.js');
 
-const programData = scrape();
-
-console.log(programData);
+scrape().then(programData => {
+    console.log('Scraped Data:', programData);
+    // You can now use programData as needed
+}).catch(error => {
+    console.error('Error during scraping:', error);
+});
