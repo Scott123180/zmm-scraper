5cbc3d926b74ed8b5924268ea2b7365cba14c68b
scott123180 <me@scotthansen.io>
2024-02-24 16:38:02 -0500

    adding tests

--- __tests__/data-processor.test.mjs
+++ __tests__/data-processor.test.mjs
@@ -5,20 +5,50 @@ describe('processProgramData', () => {
   test('should correctly process data for new programs', async () => {
     // Mock scraperDataPromise and storedDataPromise with the given data
     const scraperDataPromise = Promise.resolve([program4_raw_scraped, program5_raw_scraped]);
-    const storedDataPromise = Promise.resolve([program1_stored, program2_stored, program3_stored]);
+    const storedDataPromise = Promise.resolve([]);
 
     const { newPrograms, waitlistedPrograms, expiredPrograms } = await processProgramData(scraperDataPromise, storedDataPromise);
-
-    const expectedWaitlistedPrograms = [];
-    const expectedExpiredPrograms = []; // Assuming none of the stored programs are considered expired in this scenario
 
     expect(newPrograms[0].programId).toEqual("rs-program-id-3315");
     expect(newPrograms[0].firstSeenTimestamp).toBeGreaterThan(1708809547565);
     expect(newPrograms[1].programId).toEqual("rs-program-id-3307");
     expect(newPrograms[1].firstSeenTimestamp).toBeGreaterThan(1708809547565);
 
-    expect(waitlistedPrograms).toEqual(expect.arrayContaining(expectedWaitlistedPrograms));
-    expect(expiredPrograms).toEqual(expect.arrayContaining(expectedExpiredPrograms));
+    expect(waitlistedPrograms).toEqual([]);
+    expect(expiredPrograms).toEqual([]);
+  });
+  
+  test('should correctly process data for new waitlisted programs', async () => {
+    // Mock scraperDataPromise and storedDataPromise with the given data
+    const scraperDataPromise = Promise.resolve([program1_raw_scraped_waitlisted, program2_raw_scraped_waitlisted]);
+    const storedDataPromise = Promise.resolve([program1_stored, program2_stored]);
+
+    const { newPrograms, waitlistedPrograms, expiredPrograms } = await processProgramData(scraperDataPromise, storedDataPromise);
+
+    expect(waitlistedPrograms[0].programId).toEqual("rs-program-id-3310");
+    expect(waitlistedPrograms[0].hasRegistration).toEqual(false);
+    expect(waitlistedPrograms[0].hasWaitingList).toEqual(true);
+    expect(waitlistedPrograms[0].filledUpTimestamp).toBeGreaterThan(1708809547565);
+
+    expect(waitlistedPrograms[1].programId).toEqual("rs-program-id-3443");
+    expect(waitlistedPrograms[1].hasRegistration).toEqual(false);
+    expect(waitlistedPrograms[1].hasWaitingList).toEqual(true);
+    expect(waitlistedPrograms[1].filledUpTimestamp).toBeGreaterThan(1708809547565);
+
+    expect(newPrograms).toEqual([]);
+    expect(expiredPrograms).toEqual([]);
+  });
+  
+  test('should correctly process data for expired programs', async () => {
+    // Mock scraperDataPromise and storedDataPromise with the given data
+    const scraperDataPromise = Promise.resolve([]);
+    const storedDataPromise = Promise.resolve([program1_stored, program2_stored]);
+
+    const { newPrograms, waitlistedPrograms, expiredPrograms } = await processProgramData(scraperDataPromise, storedDataPromise);
+
+    expect(newPrograms).toEqual([]);
+    expect(waitlistedPrograms).toEqual([]);
+    expect(expiredPrograms).toEqual([program1_stored, program2_stored]);
   });
 });
 
@@ -99,6 +129,27 @@ const program3_stored = {
   "firstSeenTimestamp": 1707008510809,
   "hasWaitingList": false,
   "hasRegistration": true
+};
+
+const program1_raw_scraped_waitlisted = {
+  "programId": "rs-program-id-3310",
+  "link": "https://zmm.org/our-programs-2/3310/beginning-instruction-on-wednesday-evenings-at-zen-mountain-monastery-winter-schedule",
+  "title": "Beginning Instruction on Wednesday Evenings at Zen Mountain Monastery (winter schedule)",
+  "programDate": "February 14, 2024",
+  "programLocation": "Zen Mountain Monastery",
+  "firstSeenTimestamp": 1707008510809,
+  "hasWaitingList": true,
+  "hasRegistration": false
+};
+const program2_raw_scraped_waitlisted = {
+  "programId": "rs-program-id-3443",
+  "link": "https://zmm.org/our-programs-2/3443/ecosattva-retreat-online",
+  "title": "Ecosattva Retreat (Online)",
+  "programDate": "February 17, 2024",
+  "programLocation": "Online (Zoom link in registration email)",
+  "firstSeenTimestamp": 1707008510809,
+  "hasWaitingList": true,
+  "hasRegistration": false
 };
 
 const program1_new_waitlisted = {
